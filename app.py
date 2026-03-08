bf347ee1e14b93a2c429b35f26580f20cb346b4d
Nishank <yadavnishank09@gmail.com>
2026-03-08 14:55:41 +0100

    initial setup: data files and folder structure

---
+++ app.py
@@ -0,0 +1,3 @@
+import streamlit as st
+st.set_page_config(page_title="Froots Portfolio Dashboard", layout="wide")
+st.title("Froots Portfolio Intelligence Dashboard")
---
+++ download_data.py
@@ -0,0 +1,9 @@
+import yfinance as yf
+data_list=["AGGG.L"]
+#["VWCE.DE","EIMI.L","AGGG.L","IUSV.DE","WSML.L"]
+for tickers in data_list:
+    data= yf.download(tickers,period="max",auto_adjust=True)
+    data.to_csv(f'data/{tickers}.csv')
+ 
+
+
